6f943fafdb5adb70cf79f4025a6a5c683fda492e
JanatByakagabaBirungi <jhanniebyakagaba24@outlook.com>
2026-08-25 19:38:02 -0400

    Complete authentication modules and update home page logout script

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,16 +1,9 @@
-#from django.conf.urls.static import static
-#from django.conf import settings
 from django.urls import path
 from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    urlpatterns = [
-   path(route='login', view=views.login_user, name='login'),
-   path(route='logout', view=views.logout_request, name='logout'),
-   path(route='register', view=views.registration, name='register'),
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] 
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register'),
+]
